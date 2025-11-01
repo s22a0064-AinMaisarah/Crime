1fc67cec50b4bae9d59c4571f8cd9e6d3a2c1a94
s22a0064-AinMaisarah <s22a0064@siswa.umk.edu.my>
2025-11-01 20:56:30 +0800

    Create test.py

---
+++ test.py
@@ -0,0 +1,15 @@
+import streamlit as st
+st.set_page_config(
+    page_title="Student Survey"
+)
+visualise = st.Page('studentSurvey.py', title='Pencapaian Akademik Pelajar', icon=":material/school:")
+
+home = st.Page('home.py', title='Homepage', default=True, icon=":material/home:")
+
+pg = st.navigation(
+        {
+            "Menu": [home, visualise]
+        }
+    )
+
+pg.run()
