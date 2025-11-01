d216857fafbf4161a1244f488fd8c16738f5462b
s22a0064-AinMaisarah <s22a0064@siswa.umk.edu.my>
2025-11-01 21:18:52 +0800

    Change visualise page from studentSurvey to crime

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import streamlit as st
 st.set_page_config(
     page_title="Student Survey"
 )
-visualise = st.Page('studentSurvey.py', title='Pencapaian Akademik Pelajar', icon=":material/school:")
+visualise = st.Page('crime.py', title='Pencapaian Akademik Pelajar', icon=":material/school:")
 
 home = st.Page('home.py', title='Homepage', default=True, icon=":material/home:")
 
